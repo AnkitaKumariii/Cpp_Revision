e737819d0671e56d89bad8b11c9ae6fa3ba1049b
Ankita Kumari <213439154+AnkitaKumariii@users.noreply.github.com>
2026-03-14 10:36:24 +0000

    Sync LeetCode submission Runtime - 18 ms (13.10%), Memory - 35.5 MB (75.98%)

---
+++ solutions/1016-subarray-sums-divisible-by-k/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<int,int>mpp;
+        mpp[0] = 1;
+        int preSum = 0, cnt = 0;
+        for(int i = 0; i<n; i++) {
+            preSum += nums[i];
+            int remove = ((preSum%k)+k)%k;
+            if(mpp[remove]>=1)
+            cnt += mpp[remove];
+            mpp[remove]++;
+        }
+        return cnt;
+    }
+};
