71eb4dbe81b156f1ae4926f7b0b88a728c5de098
Ankita Kumari <213439154+AnkitaKumariii@users.noreply.github.com>
2026-03-19 10:40:28 +0000

    Sync LeetCode submission Runtime - 23 ms (6.38%), Memory - 9.2 MB (9.67%)

---
+++ solutions/0326-power-of-three/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n == 1) return true;
+        int low = 1; 
+        int high = n/3;
+        while(low <= high) {
+            int mid = (low + high)/2;
+            if(pow(3, mid) == n) return true;
+            else if(pow(3, mid) > n) high = mid - 1;
+            else low = mid + 1;
+        }
+        return false;
+    }
+};
