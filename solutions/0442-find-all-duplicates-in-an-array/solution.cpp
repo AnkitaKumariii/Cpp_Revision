0690b86db07903017a6769e0616e246558c94bc9
Ankita Kumari <213439154+AnkitaKumariii@users.noreply.github.com>
2026-03-20 16:29:47 +0000

    Sync LeetCode submission Runtime - 3 ms (73.12%), Memory - 48.5 MB (61.76%)

---
+++ solutions/0442-find-all-duplicates-in-an-array/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+        for(int i = 0; i<n; i++) {
+            int val = abs(nums[i]);
+            if(nums[val - 1] < 0) {
+                ans.push_back(val);
+            }
+            else {
+                nums[val - 1] = - nums[val - 1];
+            }
+        }
+        return ans;
+    }
+};
