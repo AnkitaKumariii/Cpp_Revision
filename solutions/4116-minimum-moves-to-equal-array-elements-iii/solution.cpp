350292c8c694270c0be239f46dc6b4d49d6e4884
Ankita Kumari <213439154+AnkitaKumariii@users.noreply.github.com>
2026-04-05 18:16:16 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 31.6 MB (54.18%)

---
+++ solutions/4116-minimum-moves-to-equal-array-elements-iii/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int minMoves(vector<int>& nums) {
+        int n = nums.size();
+        int maxVal = *max_element(nums.begin(), nums.end());
+        long long sum = 0;
+        for (int x : nums) sum += x;
+        return (long long)maxVal * n - sum;
+    }
+};
