d8929d2a8cb63a464c9c984342c49135255b4c03
Ankita Kumari <213439154+AnkitaKumariii@users.noreply.github.com>
2026-06-04 18:10:41 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.5 MB (78.54%)

---
+++ solutions/0114-flatten-binary-tree-to-linked-list/solution.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution { // MORRIS TRAVERSAL
+public:
+    void flatten(TreeNode* root) {
+        TreeNode* curr = root;
+        while (curr) {
+            if (curr->left) {
+                TreeNode* prev = curr->left;
+                while (prev->right) {
+                    prev = prev->right;
+                }
+                prev->right = curr->right;
+                curr->right = curr->left;
+                curr->left = nullptr; // hmesha left ko null banate jayenge
+            }
+            curr = curr->right; // move to next node
+        }
+    }
+};
