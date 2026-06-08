d0f565399ff747d9774c4e4c72ea8313931f2e6d
Ankita Kumari <213439154+AnkitaKumariii@users.noreply.github.com>
2026-06-08 06:39:44 +0000

    Sync LeetCode submission Runtime - 15 ms (11.76%), Memory - 133.5 MB (49.74%)

--- solutions/2265-partition-array-according-to-given-pivot/solution.cpp
+++ solutions/2265-partition-array-according-to-given-pivot/solution.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-
-        vector<int> ans;
-        ans.reserve(nums.size());
-
-        for(int x : nums)
-            if(x < pivot) ans.push_back(x);
-
-        for(int x : nums)
-            if(x == pivot) ans.push_back(x);
-
-        for(int x : nums)
-            if(x > pivot) ans.push_back(x);
-
+        vector<int>ans ;
+        for(auto x : nums)
+        {
+            if(x < pivot)
+            ans.push_back(x);
+        }
+        for(auto x : nums)
+        {
+            if(x == pivot)
+            ans.push_back(x);
+        }
+        for(auto x : nums)
+        {
+            if(x >pivot)
+            ans.push_back(x);
+        }
         return ans;
     }
 };
