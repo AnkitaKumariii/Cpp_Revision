476f1a6eab0b3475be232ba6464b10f6624fd3c2
Ankita Kumari <213439154+AnkitaKumariii@users.noreply.github.com>
2026-06-23 15:25:56 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.8 MB (29.32%)

---
+++ solutions/0045-jump-game-ii/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        int jump = 0;
+        int l = 0, r = 0;
+        int farthest = 0;
+        while(r < n-1) {
+            for(int i = l; i<=r; i++) {
+                farthest = max(farthest, i + nums[i]);
+            }
+            l = r + 1;
+            r = farthest;
+            jump++;
+        }
+        return jump;
+    }
+};
