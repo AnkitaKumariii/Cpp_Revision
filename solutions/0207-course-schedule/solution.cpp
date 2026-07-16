e8c039a55d146c08b25ff34bfa7da8b0b9cd746f
Ankita Kumari <213439154+AnkitaKumariii@users.noreply.github.com>
2026-07-16 15:38:29 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (55.36%)

---
+++ solutions/0207-course-schedule/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>>adj(numCourses);
+        vector<int>indeg(numCourses);
+        vector<int>topo;
+        queue<int>q;
+        for(auto &it: prerequisites) {
+            indeg[it[0]]++;
+            adj[it[1]].push_back(it[0]);
+        }
+        for(int i = 0; i<numCourses; i++) {
+            if(indeg[i] == 0) {
+                q.push(i);
+            }
+        }
+        while(!q.empty()) {
+            int n = q.front();
+            q.pop();
+            topo.push_back(n);
+
+            for(int neigh: adj[n]) {
+                if(indeg[neigh] > 0) {
+                    indeg[neigh]--;
+                    if(indeg[neigh] == 0) q.push(neigh);
+                }
+            }
+        }
+        return topo.size() == numCourses;
+    }
+};
