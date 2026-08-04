49309e579290e1c54f31e2bc4c4a042b01573a58
Ankita Kumari <213439154+AnkitaKumariii@users.noreply.github.com>
2026-08-04 07:10:48 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 32.1 MB (91.52%)

---
+++ solutions/4107-find-missing-elements/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        vector<int>ans;
+        int min = nums[0];
+        int max = nums[n-1];
+        int j = 0;
+        for(int i = min; i<=max; i++) {
+            if(j<n && nums[j] == i) {
+                j++;
+            }
+            else ans.push_back(i);
+        }
+        return ans;
+    }
+};
