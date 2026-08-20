797cc805c121e8e5864ebcf8a6e9297d5aefb4dc
Ankita Kumari <213439154+AnkitaKumariii@users.noreply.github.com>
2026-08-20 10:39:04 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 25.8 MB (93.59%)

---
+++ solutions/0042-trapping-rain-water/solution.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int lmax = 0, rmax = 0;
+        int total = 0;
+        int l = 0, r = n-1;
+        while(l<r) {
+            if(height[l] <= height[r]) {
+                if(lmax < height[l]) {
+                    lmax = height[l];
+                }
+                else {
+                    total += lmax - height[l];
+                }
+                l++;
+            }
+            else {
+                if(rmax < height[r]) {
+                    rmax = height[r];
+                }
+                else {
+                    total += rmax - height[r];
+                }
+                r--;
+            }
+        }
+        return total;
+    }
+};
