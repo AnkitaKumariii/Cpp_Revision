0e53d0b81ad3abbde7495a21c463408483f36ed1
Ankita Kumari <213439154+AnkitaKumariii@users.noreply.github.com>
2026-08-20 07:12:57 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.9 MB (77.63%)

---
+++ solutions/0125-valid-palindrome/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n = s.size();
+        int l = 0;
+        int r = n-1;
+        while(l<r) {
+            while(l < r && !isalnum(s[l])) {
+                l += 1;
+            }
+            while(r > l && !isalnum(s[r])) {
+                r -= 1;
+            }
+            if(tolower(s[l]) != tolower(s[r])) {
+                return false;
+            }
+            l = l+1, r = r-1;
+        }
+        return true;
+    }
+};
